cb0c502ee8eda8efad2b2a100cd3a85c3c2d3209
Kent Martin <kentaasvang@gmail.com>
2019-04-13 13:06:58 +0200

    profile of spacy-function

---
+++ profiling_backend.py
@@ -0,0 +1,25 @@
+import cProfile
+import pstats
+import io
+from pstats import SortKey
+from server.back_end_processor import spacy as sp
+
+
+if __name__ == "__main__":
+    # constants 
+    STATS_FILE = "backend_profiling.txt"
+   
+    """ produce data """
+#    pr = cProfile.Profile()
+#    pr.enable()
+#    sp()
+#    pr.disable()
+#    pr.dump_stats(STATS_FILE)
+
+    """ print out and format data """
+    sortby = SortKey.TIME           # sort by time
+    ps = pstats.Stats(STATS_FILE)   # read file
+    ps = ps.strip_dirs()            # strip leading directories
+    ps = ps.sort_stats(sortby)
+    ps.print_stats(10)              # print out ten worst candidates
+    
